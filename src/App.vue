ee24fa7dd176bf8935f6e9704004ac7c9f1a7d5d
Wilson Novaes <larissanovaes00@hotmail.com>
2020-03-28 23:04:22 +0000

    initial files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Nav />
+  </div>
+</template>
+
+<script>
+import Nav from "./components/Nav/Nav.vue";
+
+export default {
+  name: "App",
+  components: {
+    Nav
+  }
+};
+</script>
+
+<style lang="scss">
+  @import "/assets/sass/generics";
+  @import '~bootstrap/scss/bootstrap';
+  @import '~bootstrap-vue/src/index.scss';
+</style>
---
+++ src/components/Nav/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <nav class="container p-0 d-flex justify-content-between">
+      <div class="col-12 col-sm-4 col-lg-3 d-flex justify-content-center justify-content-sm-start align-items-center">
+        <img class="logo" src="../../assets/img/logo.png" alt="logo" />
+      </div>
+      <div class="d-none d-xl-flex col-9">
+        <ul class="col-12 d-flex justify-content-between align-items-center mb-0">
+          <li class="m-20 right">Início</li>
+          <li class="m-20 right">Nossas Suítes</li>
+          <li class="m-20 right">O Motel</li>
+          <li class="m-20 right">Cortesias</li>
+          <li class="m-20 right">Contato</li>
+          <li>Localização</li>
+        </ul>
+      </div>
+      <div class=" d-none d-sm-flex facebook-icon-block">
+        <img src="../../assets/img/facebook.svg" alt="facebook" />
+      </div>
+    </nav>
+  </div>
+</template>
+<style lang="scss">
+  @import './Nav';
+</style>
